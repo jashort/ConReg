b39158f6d6b2e5312f905c2d3e934e66f4800f33
Jason Short <jason@sheersky.com>
2014-12-12 22:05:41 -0800

    Vagrant will now create MySQL user and database, write configuration variables to /etc/apache2/conf.d/conreg.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,44 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
-apt-get install -y libapache2-mod-php5 php5-mysql mysql-server php-db
+DB_HOSTNAME="localhost"       # Database Server Name
+MYSQL_ROOT_PWD="CHANGEME"     # MySQL Root Passowrd
+APP_DB_USER="kumoricon_rw"    # App level database username
+APP_DB_PWD="CHANGEME"         # App level database password
+APP_DATABASE="registration"   # Database Name
 
+echo Updating package list
+apt-get update > /dev/null
+echo Instaling debconf-utils
+apt-get install debconf-utils -y > /dev/null
 
+echo Setting MySQL root password
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PWD"
+
+echo Installing MySQL
+apt-get install mysql-server -y > /dev/null
+
+echo Dropping database
+mysql -uroot -p$MYSQL_ROOT_PWD -h$DB_HOSTNAME -e "DROP USER '$APP_DB_USER'@'$DB_HOSTNAME'"
+mysql -uroot -p$MYSQL_ROOT_PWD -h$DB_HOSTNAME -e "DROP DATABASE $APP_DATABASE IF EXISTS"
+echo Creating database
+mysql -uroot -p$MYSQL_ROOT_PWD -h$DB_HOSTNAME -e "CREATE DATABASE $APP_DATABASE"
+mysql -uroot -p$MYSQL_ROOT_PWD -h$DB_HOSTNAME -e "grant SELECT, UPDATE, DELETE on $APP_DATABASE.* to '$APP_DB_USER'@'localhost' identified by '$APP_DB_PWD'"
+
+echo Installing Apache and PHP
+apt-get install -y apache2 libapache2-mod-php5 php5-mysql php-db > /dev/null
+
+echo Mounting shared directories
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant-site /var/www
 fi
 
+echo Writing configuration environment variables to /etc/apache2/conf.d/conreg.conf
+echo "# ConReg database connection information" > /etc/apache2/conf.d/conreg.conf
+echo "SetEnv REG_DB_HOSTNAME '$DB_HOSTNAME'" >> /etc/apache2/conf.d/conreg.conf
+echo "SetEnv REG_DB_NAME '$APP_DATABASE'" >> /etc/apache2/conf.d/conreg.conf
+echo "SetEnv REG_DB_USER '$APP_DB_USER'" >> /etc/apache2/conf.d/conreg.conf
+echo "SetEnv REG_DB_PASS '$APP_DB_PWD'" >> /etc/apache2/conf.d/conreg.conf
+
+apache2ctl restart
