ebb508a939c46a0d72af43452b7c36f00f145ac8
Jason Short <jason@sheersky.com>
2014-12-17 22:55:48 -0800

    Added missing field bnumber (badge number) to kumo_reg_staff table Added kumo_reg_data (attendee data) table

--- install/01-tables.sql
+++ install/01-tables.sql
@@ -11,10 +11,39 @@ CREATE TABLE registration.kumo_reg_staff
     kumo_reg_staff_lname VARCHAR(60) NOT NULL,
     kumo_reg_staff_initials CHAR(3) NOT NULL,
     kumo_reg_staff_phone_number VARCHAR(60),
+    kumo_reg_staff_bnumber INT DEFAULT 1 NOT NULL COMMENT 'Next Badge Number',
     kumo_reg_staff_accesslevel INT DEFAULT 1 NOT NULL
 );
 ALTER TABLE registration.kumo_reg_staff ADD CONSTRAINT unique_kumo_reg_staff_id UNIQUE (kumo_reg_staff_id);
+ALTER TABLE registration.kumo_reg_staff ADD CONSTRAINT unique_kumo_reg_staff_bnumber UNIQUE (kumo_reg_staff_bnumber);
 ALTER TABLE registration.kumo_reg_staff ADD CONSTRAINT unique_kumo_reg_staff_initials UNIQUE (kumo_reg_staff_initials);
+
+
+CREATE TABLE registration.kumo_reg_data
+(
+    kumo_reg_data_id         INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    kumo_reg_data_fname      VARCHAR(60)     NOT NULL,
+    kumo_reg_data_lname      VARCHAR(60)     NOT NULL,
+    kumo_reg_data_bnumber    VARCHAR(10),
+    kumo_reg_data_phone      VARCHAR(60),
+    kumo_reg_data_zip        VARCHAR(10),
+    kumo_reg_data_email      VARCHAR(250),
+    kumo_reg_data_bdate      DATE,
+    kumo_reg_data_ecfullname VARCHAR(250),
+    kumo_reg_data_ecphone    VARCHAR(250),
+    kumo_reg_data_same       BOOLEAN,
+    kumo_reg_data_parent     VARCHAR(250),
+    kumo_reg_data_parentphone VARCHAR(60),
+    kumo_reg_data_parentform BOOLEAN,
+    kumo_reg_data_paid       BOOLEAN,
+    kumo_reg_data_paidamount DECIMAL(5, 2),
+    kumo_reg_data_passtype   VARCHAR(50),
+    kumo_reg_data_regtype    VARCHAR(50),
+    kumo_reg_data_paytype    VARCHAR(50),
+    kumo_reg_data_checkedin  BOOLEAN,
+    kumo_reg_data_notes      TEXT,
+    kumo_reg_data_staff_add  VARCHAR(60)
+);
 
 
 CREATE TABLE registration.orders
