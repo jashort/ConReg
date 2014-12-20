95d0a1b12326afe63523355ef72cf236d1d22d7c
Jason Short <jason@sheersky.com>
2014-12-19 23:20:04 -0800

    Cleanup in csv import file

--- site/admin/csvimport.php
+++ site/admin/csvimport.php
@@ -2,10 +2,10 @@
 <?php require_once('../includes/functions.php'); ?>
 <?php 
 
-if ($_FILES[csv][size] > 0) {
+if ($_FILES && $_FILES['csv']['size'] > 0) {
 
     //get the csv file
-    $file = $_FILES[csv][tmp_name];
+    $file = $_FILES['csv']['tmp_name'];
     $handle = fopen($file,"r");
 	
 	$stmt = $conn->prepare("SELECT kumo_reg_staff_bnumber FROM kumo_reg_staff WHERE kumo_reg_staff_username = :uname");
@@ -14,49 +14,50 @@ if ($_FILES[csv][size] > 0) {
 	$BNumber = $results['kumo_reg_staff_bnumber']+1;
     
     //loop through the csv file and insert into database
-    do {
-        if ($data[0]) {
-		
-		$name = explode(" ",$data[0]);
-		$FirstName = $name[0];
-		$LastName = $name[1];
-		
-		$BadgeNumber = "ONL" . $BNumber;
-		
-		$PhoneNumber = $data[17];
-		$Phone_Stripped = preg_replace("/[^a-zA-Z0-9s]/","",$PhoneNumber);
-		
-		$date = explode("/",$data[18]);
-		$Month = str_pad($date[0], 2, "0", STR_PAD_LEFT);
-		$Day = str_pad($date[1], 2, "0", STR_PAD_LEFT);
-		$Year = $date[2];
-		$BDate = $Year . "-" . $Month . "-" . $Day;
-		
-		switch ($data[7]) {
-    		case "Young Child Membership":
-        		$Amount = 0;
-        		break;
-    		case "Child Membership":
-        		$Amount = 35;
-        		break;
-   			case "Standard Membership":
-				$Amount = 55;
-				break;
-    		case "VIP Membership":
-				$Amount = 300;
-				break;
+	$count = 0;
+
+	while (($data = fgetcsv($handle,1000,",","'")) !== FALSE) {
+
+        if (count($data) > 1) {
+			$name = explode(" ",$data[0]);
+			$FirstName = $name[0];
+			$LastName = $name[1];
+
+			$BadgeNumber = "ONL" . $BNumber;
+
+			$PhoneNumber = $data[17];
+			$Phone_Stripped = preg_replace("/[^a-zA-Z0-9s]/","",$PhoneNumber);
+
+			$date = explode("/",$data[18]);
+			$Month = str_pad($date[0], 2, "0", STR_PAD_LEFT);
+			$Day = str_pad($date[1], 2, "0", STR_PAD_LEFT);
+			$Year = $date[2];
+			$BDate = $Year . "-" . $Month . "-" . $Day;
+
+			switch ($data[7]) {
+				case "Young Child Membership":
+					$Amount = 0;
+					break;
+				case "Child Membership":
+					$Amount = 35;
+					break;
+				case "Standard Membership":
+					$Amount = 55;
+					break;
+				case "VIP Membership":
+					$Amount = 300;
+					break;
+			}
+			
+			$stmt = $conn->prepare("INSERT INTO kumo_reg_data (kumo_reg_data_fname, kumo_reg_data_lname, kumo_reg_data_bnumber, kumo_reg_data_bname, kumo_reg_data_address, kumo_reg_data_city, kumo_reg_data_state, kumo_reg_data_zip, kumo_reg_data_phone, kumo_reg_data_email, kumo_reg_data_bdate, kumo_reg_data_ecfullname, kumo_reg_data_ecphone, kumo_reg_data_same, kumo_reg_data_parent, kumo_reg_data_parentphone, kumo_reg_data_parentform, kumo_reg_data_paid, kumo_reg_data_paidamount, kumo_reg_data_passtype, kumo_reg_data_regtype, kumo_reg_data_paytype, kumo_reg_data_checkedin, kumo_reg_data_staff_add) VALUES (:firstname, :lastname, :bnumber, :bname, :address, :city, :state, :zip, :phone, :email, :bdate, :ecname, :ecphone, 'No', :pcname, :pcphone, 'No', 'Yes', :amount, 'Weekend', 'PreReg', 'Credit/Debit', 'No', 'ONLINE')");
+			$stmt->execute(array('firstname' => $FirstName, 'lastname' => $LastName, 'bnumber' => $BadgeNumber, 'bname' => $data[11], 'address' => $data[12], 'city' => $data[13], 'state' => $data[14],'zip' => $data[15], 'phone' => $Phone_Stripped, 'email' => $data[17], 'bdate' => $BDate, 'ecname' => $data[20], 'ecphone' => $data[21], 'pcname' => $data[22], 'pcphone' => $data[23], 'amount' => $Amount));
+			$count += 1;
 		}
-			
-		$stmt = $conn->prepare("INSERT INTO kumo_reg_data (kumo_reg_data_fname, kumo_reg_data_lname, kumo_reg_data_bnumber, kumo_reg_data_bname, kumo_reg_data_address, kumo_reg_data_city, kumo_reg_data_state, kumo_reg_data_zip, kumo_reg_data_phone, kumo_reg_data_bdate, kumo_reg_data_ecfullname, kumo_reg_data_ecphone, kumo_reg_data_same, kumo_reg_data_parent, kumo_reg_data_parentphone, kumo_reg_data_parentform, kumo_reg_data_paid, kumo_reg_data_paidamount, kumo_reg_data_passtype, kumo_reg_data_regtype, kumo_reg_data_paytype, kumo_reg_data_checkedin, kumo_reg_data_staff_add) VALUES (:firstname, :lastname, :bnumber, :bname, :address, :city, :state, :zip, :phone, :bdate, :ecname, :ecphone, 'No', :pcname, :pcphone, 'No', 'Yes', :amount, 'Weekend', 'PreReg', 'Credit/Debit', 'No', 'ONLINE')");			   
-		$stmt->execute(array('firstname' => $FirstName, 'lastname' => $LastName, 'bnumber' => $BadgeNumber, 'bname' => $data[11], 'address' => $data[12], 'city' => $data[13], 'state' => $data[14],'zip' => $data[15], 'phone' => $Phone_Stripped, 'bdate' => $BDate, 'ecname' => $data[20], 'ecphone' => $data[21], 'pcname' => $data[22], 'pcphone' => $data[23], 'amount' => $Amount));
-			
-        }
 		$BNumber++;
-    } while ($data = fgetcsv($handle,1000,",","'"));
-    //
+    }
 
     //redirect
-    header('Location: csvimport.php?success=1'); die;
+    header('Location: csvimport.php?success=1&count=' . $count); die;
 
 }
 
@@ -135,9 +136,10 @@ function MM_goToURL() { //v3.0
 </div> 
 <div id="content"><!-- InstanceBeginEditable name="Content" -->
 <div>
-<?php if (!empty($_GET[success])) { echo "<b>Your file has been imported.</b><br><br>"; } //generic success notice ?>
+<?php if (!empty($_GET['success'])) { echo $_GET['count'] . " lines imported.</b><br><br>"; } // success notice ?>
 <form action="" method="post" enctype="multipart/form-data" name="form1" id="form1">
-  Choose your file: <br />
+	<p>Import Pre-registered Attendees</p>
+  Choose CSV file: <br />
   <input name="csv" type="file" id="csv" />
   <input type="submit" name="Submit" value="Submit" />
 </form>
