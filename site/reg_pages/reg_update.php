8ff3b37c5ed8579dcb6f695689377bd13be772c4
Jason Short <jason@sheersky.com>
2014-12-23 23:13:15 -0800

    Changed birthday select field to numeric input boxes

--- site/reg_pages/reg_update.php
+++ site/reg_pages/reg_update.php
@@ -217,11 +217,11 @@ else if (document.reg_add3.PassType_4.checked) {
 <?php } ?>
 <label>Zip :
 <input name="Zip" type="text" class="input_20_150" id="Zip" value="<?php echo $results['kumo_reg_data_zip'] ?>"  /></label>
-<label>Country :
 <?php if($results['kumo_reg_data_address']!="") { ?>
-<input name="Country" type="text" class="input_20_150" id="Country" value="<?php echo $results['kumo_reg_data_country'] ?>"  />
-<br />
+	<label>Country :
+	<input name="Country" type="text" class="input_20_150" id="Country" value="<?php echo $results['kumo_reg_data_country'] ?>"  />
 <?php } ?></label>
+<br />
 <label>E-Mail :
 <input name="EMail" type="text" class="input_20_200" id="E-Mail" value="<?php echo $results['kumo_reg_data_email'] ?>"  /></label>
 <label>E-Mail Verification :
@@ -230,162 +230,12 @@ else if (document.reg_add3.PassType_4.checked) {
 <label>Phone Number:
 <input name="PhoneNumber" type="text" class="input_20_200" id="PhoneNumber" value="<?php echo $results['kumo_reg_data_phone'] ?>"  /></label>
 <label>Birth Date:
-<select name="BirthMonth" class="select_25_50" id="Birth Month" >
-<option value="" <?php if ($BirthMonth == "") echo "selected=\"selected\""; ?> >MM</option>
-<option value="01" <?php if ($BirthMonth == "01") echo "selected=\"selected\""; ?> >01</option>
-<option value="02" <?php if ($BirthMonth == "02") echo "selected=\"selected\""; ?> >02</option>
-<option value="03" <?php if ($BirthMonth == "03") echo "selected=\"selected\""; ?> >03</option>
-<option value="04" <?php if ($BirthMonth == "04") echo "selected=\"selected\""; ?> >04</option>
-<option value="05" <?php if ($BirthMonth == "05") echo "selected=\"selected\""; ?> >05</option>
-<option value="06" <?php if ($BirthMonth == "06") echo "selected=\"selected\""; ?> >06</option>
-<option value="07" <?php if ($BirthMonth == "07") echo "selected=\"selected\""; ?> >07</option>
-<option value="08" <?php if ($BirthMonth == "08") echo "selected=\"selected\""; ?> >08</option>
-<option value="09" <?php if ($BirthMonth == "09") echo "selected=\"selected\""; ?> >09</option>
-<option value="10" <?php if ($BirthMonth == "10") echo "selected=\"selected\""; ?> >10</option>
-<option value="11" <?php if ($BirthMonth == "11") echo "selected=\"selected\""; ?> >11</option>
-<option value="12" <?php if ($BirthMonth == "12") echo "selected=\"selected\""; ?> >12</option>
-</select>
-
+	<input type="number" class="input_20_40" maxlength="2" name="BirthMonth" id="Birth Month" value="<?php echo $BirthMonth?>" min="1" max="12" placeholder="MM">
 	<span class="bold_text">/</span>
-<select name="BirthDay" class="select_25_50" id="Birth Day"  >
-	<option value="">DD</option>
-<option value="01" <?php if ($BirthDay == "01") echo "selected=\"selected\""; ?> >01</option>
-<option value="02" <?php if ($BirthDay == "02") echo "selected=\"selected\""; ?> >02</option>
-<option value="03" <?php if ($BirthDay == "03") echo "selected=\"selected\""; ?> >03</option>
-<option value="04" <?php if ($BirthDay == "04") echo "selected=\"selected\""; ?> >04</option>
-<option value="05" <?php if ($BirthDay == "05") echo "selected=\"selected\""; ?> >05</option>
-<option value="06" <?php if ($BirthDay == "06") echo "selected=\"selected\""; ?> >06</option>
-<option value="07" <?php if ($BirthDay == "07") echo "selected=\"selected\""; ?> >07</option>
-<option value="08" <?php if ($BirthDay == "08") echo "selected=\"selected\""; ?> >08</option>
-<option value="09" <?php if ($BirthDay == "09") echo "selected=\"selected\""; ?> >09</option>
-<option value="10" <?php if ($BirthDay == "10") echo "selected=\"selected\""; ?> >10</option>
-<option value="11" <?php if ($BirthDay == "11") echo "selected=\"selected\""; ?> >11</option>
-<option value="12" <?php if ($BirthDay == "12") echo "selected=\"selected\""; ?> >12</option>
-<option value="13" <?php if ($BirthDay == "13") echo "selected=\"selected\""; ?> >13</option>
-<option value="14" <?php if ($BirthDay == "14") echo "selected=\"selected\""; ?> >14</option>
-<option value="15" <?php if ($BirthDay == "15") echo "selected=\"selected\""; ?> >15</option>
-<option value="16" <?php if ($BirthDay == "16") echo "selected=\"selected\""; ?> >16</option>
-<option value="17" <?php if ($BirthDay == "17") echo "selected=\"selected\""; ?> >17</option>
-<option value="18" <?php if ($BirthDay == "18") echo "selected=\"selected\""; ?> >18</option>
-<option value="19" <?php if ($BirthDay == "19") echo "selected=\"selected\""; ?> >19</option>
-<option value="20" <?php if ($BirthDay == "20") echo "selected=\"selected\""; ?> >20</option>
-<option value="21" <?php if ($BirthDay == "21") echo "selected=\"selected\""; ?> >21</option>
-<option value="22" <?php if ($BirthDay == "22") echo "selected=\"selected\""; ?> >22</option>
-<option value="23" <?php if ($BirthDay == "23") echo "selected=\"selected\""; ?> >23</option>
-<option value="24" <?php if ($BirthDay == "24") echo "selected=\"selected\""; ?> >24</option>
-<option value="25" <?php if ($BirthDay == "25") echo "selected=\"selected\""; ?> >25</option>
-<option value="26" <?php if ($BirthDay == "26") echo "selected=\"selected\""; ?> >26</option>
-<option value="27" <?php if ($BirthDay == "27") echo "selected=\"selected\""; ?> >27</option>
-<option value="28" <?php if ($BirthDay == "28") echo "selected=\"selected\""; ?> >28</option>
-<option value="29" <?php if ($BirthDay == "29") echo "selected=\"selected\""; ?> >29</option>
-<option value="30" <?php if ($BirthDay == "30") echo "selected=\"selected\""; ?> >30</option>
-<option value="31" <?php if ($BirthDay == "31") echo "selected=\"selected\""; ?> >31</option>
-</select>
-<span class="bold_text">/</span>
-<select name="BirthYear" class="select_25_75"  id="Birth Year" >
-	<option <?php if ($BirthYear == "") echo "selected=\"selected\""; ?> >YYYY</option>
-	<option value="2012" <?php if ($BirthYear == "2012") echo "selected=\"selected\""; ?> >2012</option>
-	<option value="2011" <?php if ($BirthYear == "2011") echo "selected=\"selected\""; ?> >2011</option>
-	<option value="2010" <?php if ($BirthYear == "2010") echo "selected=\"selected\""; ?> >2010</option>
-	<option value="2009" <?php if ($BirthYear == "2009") echo "selected=\"selected\""; ?> >2009</option>
-	<option value="2008" <?php if ($BirthYear == "2008") echo "selected=\"selected\""; ?> >2008</option>
-	<option value="2007" <?php if ($BirthYear == "2007") echo "selected=\"selected\""; ?> >2007</option>
-	<option value="2006" <?php if ($BirthYear == "2006") echo "selected=\"selected\""; ?> >2006</option>
-	<option value="2005" <?php if ($BirthYear == "2005") echo "selected=\"selected\""; ?> >2005</option>
-	<option value="2004" <?php if ($BirthYear == "2004") echo "selected=\"selected\""; ?> >2004</option>
-	<option value="2003" <?php if ($BirthYear == "2003") echo "selected=\"selected\""; ?> >2003</option>
-	<option value="2002" <?php if ($BirthYear == "2002") echo "selected=\"selected\""; ?> >2002</option>
-	<option value="2001" <?php if ($BirthYear == "2001") echo "selected=\"selected\""; ?> >2001</option>
-	<option value="2000" <?php if ($BirthYear == "2000") echo "selected=\"selected\""; ?> >2000</option>
-	<option value="1999" <?php if ($BirthYear == "1999") echo "selected=\"selected\""; ?> >1999</option>
-	<option value="1998" <?php if ($BirthYear == "1998") echo "selected=\"selected\""; ?> >1998</option>
-	<option value="1997" <?php if ($BirthYear == "1997") echo "selected=\"selected\""; ?> >1997</option>
-	<option value="1996" <?php if ($BirthYear == "1996") echo "selected=\"selected\""; ?> >1996</option>
-	<option value="1995" <?php if ($BirthYear == "1995") echo "selected=\"selected\""; ?> >1995</option>
-	<option value="1994" <?php if ($BirthYear == "1994") echo "selected=\"selected\""; ?> >1994</option>
-	<option value="1993" <?php if ($BirthYear == "1993") echo "selected=\"selected\""; ?> >1993</option>
-	<option value="1992" <?php if ($BirthYear == "1992") echo "selected=\"selected\""; ?> >1992</option>
-	<option value="1991" <?php if ($BirthYear == "1991") echo "selected=\"selected\""; ?> >1991</option>
-	<option value="1990" <?php if ($BirthYear == "1990") echo "selected=\"selected\""; ?> >1990</option>
-	<option value="1989" <?php if ($BirthYear == "1989") echo "selected=\"selected\""; ?> >1989</option>
-	<option value="1988" <?php if ($BirthYear == "1988") echo "selected=\"selected\""; ?> >1988</option>
-	<option value="1987" <?php if ($BirthYear == "1987") echo "selected=\"selected\""; ?> >1987</option>
-	<option value="1986" <?php if ($BirthYear == "1986") echo "selected=\"selected\""; ?> >1986</option>
-	<option value="1985" <?php if ($BirthYear == "1985") echo "selected=\"selected\""; ?> >1985</option>
-	<option value="1984" <?php if ($BirthYear == "1984") echo "selected=\"selected\""; ?> >1984</option>
-	<option value="1983" <?php if ($BirthYear == "1983") echo "selected=\"selected\""; ?> >1983</option>
-	<option value="1982" <?php if ($BirthYear == "1982") echo "selected=\"selected\""; ?> >1982</option>
-	<option value="1981" <?php if ($BirthYear == "1981") echo "selected=\"selected\""; ?> >1981</option>
-	<option value="1980" <?php if ($BirthYear == "1980") echo "selected=\"selected\""; ?> >1980</option>
-	<option value="1979" <?php if ($BirthYear == "1979") echo "selected=\"selected\""; ?> >1979</option>
-	<option value="1978" <?php if ($BirthYear == "1978") echo "selected=\"selected\""; ?> >1978</option>
-	<option value="1977" <?php if ($BirthYear == "1977") echo "selected=\"selected\""; ?> >1977</option>
-	<option value="1976" <?php if ($BirthYear == "1976") echo "selected=\"selected\""; ?> >1976</option>
-	<option value="1975" <?php if ($BirthYear == "1975") echo "selected=\"selected\""; ?> >1975</option>
-	<option value="1974" <?php if ($BirthYear == "1974") echo "selected=\"selected\""; ?> >1974</option>
-	<option value="1973" <?php if ($BirthYear == "1973") echo "selected=\"selected\""; ?> >1973</option>
-	<option value="1972" <?php if ($BirthYear == "1972") echo "selected=\"selected\""; ?> >1972</option>
-	<option value="1971" <?php if ($BirthYear == "1971") echo "selected=\"selected\""; ?> >1971</option>
-	<option value="1970" <?php if ($BirthYear == "1970") echo "selected=\"selected\""; ?> >1970</option>
-	<option value="1969" <?php if ($BirthYear == "1969") echo "selected=\"selected\""; ?> >1969</option>
-	<option value="1968" <?php if ($BirthYear == "1968") echo "selected=\"selected\""; ?> >1968</option>
-	<option value="1967" <?php if ($BirthYear == "1967") echo "selected=\"selected\""; ?> >1967</option>
-	<option value="1966" <?php if ($BirthYear == "1966") echo "selected=\"selected\""; ?> >1966</option>
-	<option value="1965" <?php if ($BirthYear == "1965") echo "selected=\"selected\""; ?> >1965</option>
-	<option value="1964" <?php if ($BirthYear == "1964") echo "selected=\"selected\""; ?> >1964</option>
-	<option value="1963" <?php if ($BirthYear == "1963") echo "selected=\"selected\""; ?> >1963</option>
-	<option value="1962" <?php if ($BirthYear == "1962") echo "selected=\"selected\""; ?> >1962</option>
-	<option value="1961" <?php if ($BirthYear == "1961") echo "selected=\"selected\""; ?> >1961</option>
-	<option value="1960" <?php if ($BirthYear == "1960") echo "selected=\"selected\""; ?> >1960</option>
-	<option value="1959" <?php if ($BirthYear == "1959") echo "selected=\"selected\""; ?> >1959</option>
-	<option value="1958" <?php if ($BirthYear == "1958") echo "selected=\"selected\""; ?> >1958</option>
-	<option value="1957" <?php if ($BirthYear == "1957") echo "selected=\"selected\""; ?> >1957</option>
-	<option value="1956" <?php if ($BirthYear == "1956") echo "selected=\"selected\""; ?> >1956</option>
-	<option value="1955" <?php if ($BirthYear == "1955") echo "selected=\"selected\""; ?> >1955</option>
-	<option value="1954" <?php if ($BirthYear == "1954") echo "selected=\"selected\""; ?> >1954</option>
-	<option value="1953" <?php if ($BirthYear == "1953") echo "selected=\"selected\""; ?> >1953</option>
-	<option value="1952" <?php if ($BirthYear == "1952") echo "selected=\"selected\""; ?> >1952</option>
-	<option value="1951" <?php if ($BirthYear == "1951") echo "selected=\"selected\""; ?> >1951</option>
-	<option value="1950" <?php if ($BirthYear == "1950") echo "selected=\"selected\""; ?> >1950</option>
-	<option value="1949" <?php if ($BirthYear == "1949") echo "selected=\"selected\""; ?> >1949</option>
-	<option value="1948" <?php if ($BirthYear == "1948") echo "selected=\"selected\""; ?> >1948</option>
-	<option value="1947" <?php if ($BirthYear == "1947") echo "selected=\"selected\""; ?> >1947</option>
-	<option value="1946" <?php if ($BirthYear == "1946") echo "selected=\"selected\""; ?> >1946</option>
-	<option value="1945" <?php if ($BirthYear == "1945") echo "selected=\"selected\""; ?> >1945</option>
-	<option value="1944" <?php if ($BirthYear == "1944") echo "selected=\"selected\""; ?> >1944</option>
-	<option value="1943" <?php if ($BirthYear == "1943") echo "selected=\"selected\""; ?> >1943</option>
-	<option value="1942" <?php if ($BirthYear == "1942") echo "selected=\"selected\""; ?> >1942</option>
-	<option value="1941" <?php if ($BirthYear == "1941") echo "selected=\"selected\""; ?> >1941</option>
-	<option value="1940" <?php if ($BirthYear == "1940") echo "selected=\"selected\""; ?> >1940</option>
-	<option value="1939" <?php if ($BirthYear == "1939") echo "selected=\"selected\""; ?> >1939</option>
-	<option value="1938" <?php if ($BirthYear == "1938") echo "selected=\"selected\""; ?> >1938</option>
-	<option value="1937" <?php if ($BirthYear == "1937") echo "selected=\"selected\""; ?> >1937</option>
-	<option value="1936" <?php if ($BirthYear == "1936") echo "selected=\"selected\""; ?> >1936</option>
-	<option value="1935" <?php if ($BirthYear == "1935") echo "selected=\"selected\""; ?> >1935</option>
-	<option value="1934" <?php if ($BirthYear == "1934") echo "selected=\"selected\""; ?> >1934</option>
-	<option value="1933" <?php if ($BirthYear == "1933") echo "selected=\"selected\""; ?> >1933</option>
-	<option value="1932" <?php if ($BirthYear == "1932") echo "selected=\"selected\""; ?> >1932</option>
-	<option value="1931" <?php if ($BirthYear == "1931") echo "selected=\"selected\""; ?> >1931</option>
-	<option value="1930" <?php if ($BirthYear == "1930") echo "selected=\"selected\""; ?> >1930</option>
-	<option value="1929" <?php if ($BirthYear == "1929") echo "selected=\"selected\""; ?> >1929</option>
-	<option value="1928" <?php if ($BirthYear == "1928") echo "selected=\"selected\""; ?> >1928</option>
-	<option value="1927" <?php if ($BirthYear == "1927") echo "selected=\"selected\""; ?> >1927</option>
-	<option value="1926" <?php if ($BirthYear == "1926") echo "selected=\"selected\""; ?> >1926</option>
-	<option value="1925" <?php if ($BirthYear == "1925") echo "selected=\"selected\""; ?> >1925</option>
-	<option value="1924" <?php if ($BirthYear == "1924") echo "selected=\"selected\""; ?> >1924</option>
-	<option value="1923" <?php if ($BirthYear == "1923") echo "selected=\"selected\""; ?> >1923</option>
-	<option value="1922" <?php if ($BirthYear == "1922") echo "selected=\"selected\""; ?> >1922</option>
-	<option value="1921" <?php if ($BirthYear == "1921") echo "selected=\"selected\""; ?> >1921</option>
-	<option value="1920" <?php if ($BirthYear == "1920") echo "selected=\"selected\""; ?> >1920</option>
-	<option value="1919" <?php if ($BirthYear == "1919") echo "selected=\"selected\""; ?> >1919</option>
-	<option value="1918" <?php if ($BirthYear == "1918") echo "selected=\"selected\""; ?> >1918</option>
-	<option value="1917" <?php if ($BirthYear == "1917") echo "selected=\"selected\""; ?> >1917</option>
-	<option value="1916" <?php if ($BirthYear == "1916") echo "selected=\"selected\""; ?> >1916</option>
-	<option value="1915" <?php if ($BirthYear == "1915") echo "selected=\"selected\""; ?> >1915</option>
-	<option value="1914" <?php if ($BirthYear == "1914") echo "selected=\"selected\""; ?> >1914</option>
-	<option value="1913" <?php if ($BirthYear == "1913") echo "selected=\"selected\""; ?> >1913</option>
-	<option value="1912" <?php if ($BirthYear == "1912") echo "selected=\"selected\""; ?> >1912</option>
-</select></label>
+	<input type="number" class="input_20_40" maxlength="2" name="BirthDay" id="Birth Day" value="<?php echo $BirthDay?>" min="1" max="31" placeholder="DD">
+	<span class="bold_text">/</span>
+	<input type="number" class="input_20_60" maxlength="4" name="BirthYear" id="Birth Year" value="<?php echo $BirthYear?>" min="1900" max="2015" placeholder="YYYY">
+	</label>(Month / Day / Year)
 </fieldset>
 <fieldset id="emergencyinfo">
 <legend>Emergency Contact Info</legend>
