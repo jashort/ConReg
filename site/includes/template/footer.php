6245fadb6d94af1cd31a9ab57d74bb9b33b4e910
Jason Short <jason@sheersky.com>
2015-09-04 07:44:52 -0700

    Made test page link in footer an absoloute link

--- site/includes/template/footer.php
+++ site/includes/template/footer.php
@@ -1,5 +1,5 @@
 <div class="modal-footer">
-    <span style="float: left"><a href="reg_pages/badgeprinttest.php" target="_blank">Print Test Badge</a></span>
+    <span style="float: left"><a href="/reg_pages/badgeprinttest.php" target="_blank">Print Test Badge</a></span>
 
     &copy; Tim Zuidema and Jason Short
 </div>
